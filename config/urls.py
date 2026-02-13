eeb0b035f6b821d08d00cdc1d11eab93faecf852
yhlas70 <yhhykomp@gmail.com>
2026-02-13 18:54:43 +0500

    deleted 2 urls with config

--- config/urls.py
+++ config/urls.py
@@ -3,6 +3,4 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('apps.main.urls')),
-    path('user/', include('apps.users.urls')),
 ]
