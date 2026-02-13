3db55dab51562fc909c13af8d1a72d73a1a77dae
yhlas70 <yhhykomp@gmail.com>
2026-02-13 18:43:15 +0500

    verssion 2 is ready

--- config/urls.py
+++ config/urls.py
@@ -4,5 +4,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.main.urls')),
-    path('user/', include('apps.users.urls')),
 ]
